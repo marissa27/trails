7bb6f38c4e4501102a9036b8dd0d7a15a4d0e1cc
Marissa Reinke <reinke.marissa@gmail.com>
2018-02-18 13:13:38 -0700

    Setup Header component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import HomePage from '../HomePage/HomePage';
+import Header from '../Header/Header';
 import logo from '../../logo.svg';
 import './App.css';
 
@@ -7,6 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <HomePage />
       </div>
     );
