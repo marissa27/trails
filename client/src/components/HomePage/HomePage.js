93e95d65ddf06d5c4af647ed3073f17e2438b396
Marissa Reinke <reinke.marissa@gmail.com>
2018-02-18 15:59:21 -0700

    Setup search component and structure

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Search from '../Search/Search';
 
 export default class HomePage extends Component {
   render() {
     return (
       <div className="App">
         HomePage
+        <Search />
       </div>
     );
   }
