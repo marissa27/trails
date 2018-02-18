b87342c2f7648d09cef1899db8ca9b5ea56bbd4b
Marissa Reinke <reinke.marissa@gmail.com>
2018-02-18 14:28:31 -0700

    Create route and component login

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import App from './components/App/App';
+import Login from './components/Login/Login';
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={App}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/signup' component={Login}/>
+
     </Switch>
   </main>
 );
